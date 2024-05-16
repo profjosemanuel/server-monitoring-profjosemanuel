364f4d4e19eca771473b4b1f2e97a054d833b909
alumno <alumno@debian>
2024-05-16 16:34:20 +0200

    prueba

---
+++ paquetes_2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+PATH=/usr/sbin:/sbin:/usr/bin:/bin
+
+# Detect distribution, for required module install later
+# =======================================================
+
+sendmail_instalado=FALSE
+bc_instalado=FALSE
+sysstat_instaldo=FALSE
+
+
+
+# Install missing modules
+# ==========================
+if ! type "sendmail" > /dev/null; then
+          	echo "Installing Postfix for sendmail command..."
+fi
+if ! type "bc" > /dev/null; then
+          	echo "Installing bc (an arbitrary precision calculator language)..."
+fi
+if ! type "mpstat" > /dev/null; then
+          	echo "Installing sar, sadf, mpstat, iostat, pidstat and sa tools..."
+fi
